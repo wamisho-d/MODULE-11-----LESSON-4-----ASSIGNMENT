2dd4251dfdefee33d27edfd14982f8f1af7d7584
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-19 12:16:42 -0500

    Update BrowseCharacters.js

---
+++ src/# 2 pages/BrowseCharacters.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const characters = [
+  { id: 1, name: "Spider-Man" },
+  { id: 2, name: "Iron Man" },
+  { id: 3, name: "Thor" },
+];
+
+const BrowseCharacters = () => (
+  <div>
+    <h2>Browse Characters</h2>
+    <ul>
+      {characters.map((character) => (
+        <li key={character.id}>
+          <Link to={`/characters/${character.id}`}>{character.name}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default BrowseCharacters;
