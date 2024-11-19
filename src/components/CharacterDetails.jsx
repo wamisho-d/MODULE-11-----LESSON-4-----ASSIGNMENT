31fdfaf73de4ade51e6ff2c8f19f443fda72a7de
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-19 12:19:42 -0500

    Update CharacterDetails.jsx

---
+++ src/components/CharacterDetails.jsx
@@ -0,0 +1,10 @@
+const CharacterDetails = () => {
+    return (
+        <div>
+            <h2>Character Details</h2>
+            <p>Details of the selected character will appear here.</p>
+        </div>
+    );
+};
+
+export default CharacterDetails;
