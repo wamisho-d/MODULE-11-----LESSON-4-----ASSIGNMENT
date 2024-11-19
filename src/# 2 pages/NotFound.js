2427261e199d72945db6032fdc3b5e885f632c95
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-19 12:18:20 -0500

    Update NotFound.js

---
+++ src/# 2 pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+  <div>
+    <h2>Page Not Found</h2>
+    <p>Oops! The page you are looking for does not exist.</p>
+  </div>
+);
+
+export default NotFound;
