112b7636b0ed00311ace302426cf257d8bff69ac
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-19 12:19:20 -0500

    Update BrowseCharacters.jsx

---
+++ src/components/BrowseCharacters.jsx
@@ -0,0 +1,10 @@
+const BrowseCharacters = () => {
+    return (
+        <div>
+            <h2>Browse Characters</h2>
+            <p>Browse through a list of your favorite characters.</p>
+        </div>
+    );
+};
+
+export default BrowseCharacters;
